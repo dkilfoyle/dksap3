cf968b55237c48eba007d41667081a0e50902dcc
Dean Kilfoyle <dkilfoyle@xtra.co.nz>
2025-04-22 11:06:25 +1200

    test

--- apps/ide/src/examples/c/test.c
+++ apps/ide/src/examples/c/test.c
@@ -1,8 +1,13 @@
 main()
 {
-  int x;
-  x = 1;
-  if (x | 4 != 5)
-    return 5;
+  int z[8];
+  int *r;
+  r = z;
+  // sizeof only working for struct
+  // if (sizeof(z) != 16)
+  //   return 1;
+  z[0] = 1;
+  if (*r != 1)
+    return 3;
   return 0;
 }
